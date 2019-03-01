74f653ff6bb43a337b33d004a185eb082f2d0254
Nate Boyette <nateboyette@icloud.com>
2019-02-28 16:14:48 -0800

    Add register page and ability to add a new user

--- authapp/src/App.js
+++ authapp/src/App.js
@@ -3,19 +3,34 @@ import "./App.css";
 import React, { Component } from "react";
 
 
-import { NavLink, Route } from "react-router-dom";
+import { NavLink, Route, withRouter } from "react-router-dom";
 
 import Login from './components/Login';
 import Users from "./components/Users"
 import SignUp from './components/SignUp'
 
 class App extends Component {
+  
+  logout = e => {
+    e.preventDefault();
+    
+    localStorage.removeItem('jwt');
+    this.props.history.push('/login')
+
+
+  }
+  
   render() {
     return (
       <div className="App">
         <div>
           <NavLink to='/login'>Login</NavLink>
-          <NavLink to='/users'>Users</NavLink>
+          
+          {
+            !localStorage.getItem('jwt') ? null : <NavLink to='/users'>Users</NavLink>
+          }
+          {!localStorage.getItem('jwt') ? <NavLink to='/signup'>Sign Up</NavLink> : null}
+          {!localStorage.getItem('jwt') ? null : <button onClick={this.logout}>Log Out</button>}
         </div>
         <Route path='/login' component={Login} />
         <Route path='/signup' component={SignUp} />
@@ -25,4 +40,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withRouter(App);
--- authapp/src/components/SignUp.js
+++ authapp/src/components/SignUp.js
@@ -1,11 +1,72 @@
 import React from "react";
-
+import axios from 'axios';
 class SignUp extends React.Component {
+  state={
+    username: '',
+    password: '',
+    department: '',
+  }
+  
+  handleChange = e => {
+    const { name, value } = e.target;
+
+    this.setState({
+      [name]: value
+    });
+  };
+
+
+  registerUser = e => {
+    e.preventDefault();
+
+    const endpoint = 'http://localhost:8000/api/register'
+
+    axios.post(endpoint, this.state)
+    .then(res => {
+      console.log(res)
+      localStorage.setItem('jwt', res.data.token)
+      this.props.history.push('/users');
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+  
+  
   render() {
     return (
-      <>
-        <div>I'm the sign up form</div>
-      </>
+      <div>
+        <form onSubmit={this.registerUser}>
+          <div>
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="department">Department</label>
+            <input
+              type="text"
+              name="department"
+              value={this.state.department}
+              onChange={this.handleChange}
+            />
+          </div>
+          <button type="submit">Register</button>
+        </form>
+      </div>
     );
   }
 }
